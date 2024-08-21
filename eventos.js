25e3ffbb05e276cbf22a99b05adeabe861cb80a3
america <americacaceres10@gmail.com>
2024-08-21 16:07:30 -0300

    punto 5

--- eventos.js
+++ eventos.js
@@ -2,3 +2,10 @@ let div = document.getElementById("div");
 div.addEventListener("click", function(){
 alert("Hola! Soy el div")
 })
+
+
+let button = document.getElementById("button");
+button.addEventListener("click", function(event){
+    event.stopPropagation()
+    alert("Hola!")
+})
